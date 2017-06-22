99a6e45be3940dccea5a068a6f423b7878e8f9aa
Jimmy Hoang <hoangjimmy94@gmail.com>
2017-06-22 12:11:52 -0700

    Setup UI

--- Receipts++/AddReceiptViewController.h
+++ Receipts++/AddReceiptViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  AddReceiptViewController.h
 //  Receipts++
 //
 //  Created by Jimmy Hoang on 2017-06-22.
@@ -8,12 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-<
-UITableViewDelegate,
-UITableViewDataSource
->
-
+@interface AddReceiptViewController : UIViewController
 
 @end
-
--- Receipts++/ViewController.m
+++ Receipts++/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
@@ -23,6 +24,24 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+#pragma mark - Actions
+- (IBAction)addButtonPressed:(id)sender {
+}
+
+#pragma mark - UITableView
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 0;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 0;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return [[UITableViewCell alloc] init];
 }
 
 
