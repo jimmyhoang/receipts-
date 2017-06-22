fc3b203df78e078b5fa2641c8bf7e7550c412ba0
Jimmy Hoang <hoangjimmy94@gmail.com>
2017-06-22 11:38:05 -0700

    Initial commit

---
+++ Receipts++/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Receipts++
+//
+//  Created by Jimmy Hoang on 2017-06-22.
+//  Copyright © 2017 Jimmy Hoang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ Receipts++/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Receipts++
+//
+//  Created by Jimmy Hoang on 2017-06-22.
+//  Copyright © 2017 Jimmy Hoang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
